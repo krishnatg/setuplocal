7da64b2ac77ddd67ba6a3f61d04f4538d07d2d44
Krishna Tippur Gururaj <krishnatg@gmail.com>
2017-04-06 16:56:10 -0400

    added comment

--- unmuteAfterLogin.sh
+++ unmuteAfterLogin.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# copy this file to /Users/Shared/ and give it executable permission (execute chmod +x /Users/Shared/unmuteAfterLogin.sh)
+
 tempfile="/Users/Shared/.tempCurrVol.txt" ;
 status=`osascript -e 'output muted of (get volume settings)'` ;
 if [[ "$status" == "true" ]] && [[ -f $tempfile ]] ; then
